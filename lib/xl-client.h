a3cc70aa2ec17580a6806e443d1b866406f004bb
Wu Xiaotian <yetist@gmail.com>
2013-08-10 11:58:24 +0800

    commit

---
+++ lib/xl-client.h
@@ -0,0 +1,47 @@
+/* vi: set sw=4 ts=4 wrap ai: */
+/*
+ * client.h: This file is part of ____
+ *
+ * Copyright (C) 2013 yetist <xiaotian.wu@i-soft.com.cn>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * */
+
+#ifndef __CLIENT_H__ 
+#define __CLIENT_H__  1
+
+#include "xllx.h"
+#include "info.h"
+
+typedef struct _XLClient XLClient;
+
+XLClient*   xl_client_new(const char *username, const char *password);
+int         xl_client_login(XLClient *client, XLErrorCode *err);
+XLErrorCode xl_client_logout(XLClient *client);
+void        xl_client_set_verify_image_path(XLClient *client, const char *path);
+void        xl_client_set_verify_code(XLClient *client, const char *vcode);
+void        xl_client_free(XLClient *client);
+
+void xl_read_all_complete_tasks(XLClient *client);
+char *lwqq_get_cookies(XLClient *lc);
+
+int xl_add_yun_task(XLClient *client, char *url);
+char *xl_get_yun_url(XLClient *client, char *vurl, char *vname);
+
+//void lwqq_vc_free(LwqqVerifyCode *vc);
+
+
+#endif /* __CLIENT_H__ */
---
+++ lib/xl-http.h
@@ -0,0 +1,58 @@
+/* vi: set sw=4 ts=4 wrap ai: */
+/*
+ * http.h: This file is part of ____
+ *
+ * Copyright (C) 2013 yetist <xiaotian.wu@i-soft.com.cn>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * */
+
+#ifndef XL_HTTP_H
+#define XL_HTTP_H
+
+#include <ghttp.h>
+#include "xllx.h"
+
+typedef enum
+{
+	HTTP_GET = ghttp_type_get,
+	HTTP_POST = ghttp_type_post,
+} HttpMethod;
+
+typedef int (*XLAsyncCallback) (XLErrorCode ec, char *response, void* data);
+
+typedef struct _XLHttp XLHttp;
+
+XLHttp *xl_http_new(const char *url);
+XLHttp *xl_http_create_default(const char *url, XLErrorCode *err);
+
+int xl_http_open(XLHttp *request, HttpMethod method, char *body);
+int xl_http_open_async(XLHttp *request, HttpMethod method, char *body, XLAsyncCallback callback, void *data);
+
+void xl_http_set_header(XLHttp *request, const char *name, const char *value);
+
+char* xl_http_get_header(XLHttp *request, const char *name);
+char* xl_http_get_cookie(XLHttp *request, const char *name);
+int   xl_http_get_cookie_names(XLHttp *request, char ***names);
+int   xl_http_has_cookie(XLHttp *request, const char *name);
+int   xl_http_get_status(XLHttp *request);
+char* xl_http_get_body(XLHttp *request);
+int   xl_http_get_body_len(XLHttp *request);
+#define xl_http_get_response xl_http_get_body
+
+void xl_http_free(XLHttp *request);
+
+#endif  /* XL_HTTP_H */
