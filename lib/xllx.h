6fab3bbbf6b3f94b2f1a19790c4acb87df0ea653
yetist <yetist@gmail.com>
2013-08-19 17:18:02 +0800

    commit

--- lib/xllx.h
+++ lib/xllx.h
@@ -9,9 +9,9 @@
 
 #define XLLX_VERSION_MAJOR 13
 #define XLLX_VERSION_MINOR 8
-#define XLLX_VERSION_MICRO 2
+#define XLLX_VERSION_MICRO 19
 #define XLLX_VERSION_API "1"
 
-#define XLLX_VERSION "13.8.2"
+#define XLLX_VERSION "13.8.19"
 
 #endif  /* XLLX_TYPE_H */
